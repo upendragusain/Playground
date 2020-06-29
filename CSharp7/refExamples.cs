152129450f2fac5b6876a8735233ea391b4144b1
Upendra Gusain <upendra.gusain@gmail.com>
2020-06-29 17:56:58 +0100

    more ref return examples

--- CSharp7/refExamples.cs
+++ CSharp7/refExamples.cs
@@ -1,14 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace CSharp7
 {
-    public class refExamples
+    /*
+     * Members of a class can't have signatures that differ only by ref, in, or out. A compiler error occurs
+     * However, methods can be overloaded when one method has a ref, in, or out parameter and the other has a value parameter
+     * 
+     * In some cases, accessing a value by reference increases performance by avoiding a potentially expensive copy operation
+     */
+
+    public class RefExamples
     {
         public static void PassByRef(ref int id)
         {
             id = 42;
+        }
+
+        public static ref int FindLargest(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] == array.Max())
+                {
+                    return ref array[i];
+                }
+            }
+            throw new ArgumentException();
+        }
+
+        //i will give you a ref type that you can not chaage
+        public static ref readonly int RefReadonly(int[] array)
+        {
+            return ref array[0];
+        }
+    }
+
+    public class Library
+    {
+        public class Book
+        {
+            public string Title { get; set; }
+        }
+
+        private Book[] _books = {
+            new Book { Title = "Call of the Wild, The" },
+            new Book { Title = "Tale of Two Cities, A" }
+                       };
+
+        private Book nobook = null;
+
+        public Book[] ListBooks()
+        {
+            return _books;
+        }
+
+        public ref Book GetABookThatCanBeReplaced(string title)
+        {
+            for (int i = 0; i < _books.Count(); i++)
+            {
+                if(_books[i].Title == title)
+                {
+                    return ref _books[i];
+                }
+            }
+
+            return ref nobook;
         }
     }
 }
--- CSharpTests/RefTests.cs
+++ CSharpTests/RefTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using CSharp7;
 using Xunit;
+using static CSharp7.Library;
 
 namespace CSharpTests
 {
@@ -12,9 +14,43 @@ namespace CSharpTests
         public void PassByRef_updates_value()
         {
             int i = 10;
-            refExamples.PassByRef(ref i);
+            RefExamples.PassByRef(ref i);
 
             Assert.Equal(42, i);
+        }
+
+        [Fact]
+        public void Reference_Return_Value_test()
+        {
+            var array = new[] { 1, 34, 5, 78, 90, 1, 99, 100, 3, 108 };
+            ref var answer = ref RefExamples.FindLargest(array);
+
+            Assert.True(answer == 108);
+        }
+
+        [Fact]
+        public void RefReadonly_test()
+        {
+            var array = new[] { 100, 34, 5, 78, 90, 1, 99, 100, 3, 108 };
+            ref readonly int x =  ref RefExamples.RefReadonly(array);
+
+            // can't do this , compile error
+            //x++;
+            Assert.True(x == 100);
+        }
+
+        [Fact]
+        public void Library_test()
+        {
+            var lib = new Library();
+            var oldBooks = lib.ListBooks();
+
+            ref Book book = ref lib.GetABookThatCanBeReplaced("Call of the Wild, The");
+            book = new Book() { Title = "C# 7" };
+            var newBooks = lib.ListBooks();
+
+            Assert.True(newBooks.Where(_ => _.Title == "Call of the Wild, The").FirstOrDefault() == null);
+            Assert.Single(newBooks.Where(_ => _.Title == "C# 7"));
         }
     }
 }
