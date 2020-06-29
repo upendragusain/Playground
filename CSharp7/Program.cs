b24cd8ec5ac1ed37ecff9a82073f9559e825eb06
Upendra Gusain <upendra.gusain@gmail.com>
2020-06-29 15:12:48 +0100

    add tuple examples

---
+++ CSharp7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Employee.Run();
+            Console.WriteLine($"{result.Name}, {result.Age}");
+            Employee.Run2();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp7/TupleExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharp7
+{
+    public class Employee
+    {
+        public static (string Name, int Age) Run()
+        {
+            return (Name: "Upendra", Age: 42);
+        }
+
+        public static void Run2()
+        {
+            (string a, int b, double c) bunchOfThings = ("a", 1, 10.9);
+
+            Console.WriteLine($"{bunchOfThings.a}, " +
+                $"{bunchOfThings.b}, {bunchOfThings.c}");
+        }
+    }
+}
