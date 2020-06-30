a5ec9468a1607cf86f3dc0697ece9d53246dcc15
Upendra Gusain <upendra.gusain@gmail.com>
2020-06-30 11:53:52 +0100

    add default literal

---
+++ CSharp7/DefaultValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp7
+{
+    public class DefaultValue
+    {
+        public static T DisplayDefault<T>()
+        {
+            return default(T);
+        }
+    }
+}
---
+++ CSharpTests/DefaultValueTests.cs
@@ -0,0 +1,30 @@
+﻿using CSharp7;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CSharpTests
+{
+    public class DefaultValueTests
+    {
+        [Fact]
+        public void DisplayDefaultTest()
+        {
+            var val = DefaultValue.DisplayDefault<int>();
+            Assert.Equal(0, val);
+
+            var val2 = DefaultValue.DisplayDefault<int?>();
+            Assert.Null(val2);
+
+            var val3 = DefaultValue.DisplayDefault<string>();
+            Assert.Null(val3);
+
+            var val4 = DefaultValue.DisplayDefault<object>();
+            Assert.Null(val4);
+
+            var val5 = DefaultValue.DisplayDefault<List<int>>();
+            Assert.Null(val5);
+        }
+    }
+}
