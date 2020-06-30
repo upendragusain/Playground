aa1c362f71f3d34b5dc17f1bc0f1daebf89cbf17
Upendra Gusain <upendra.gusain@gmail.com>
2020-06-30 11:10:42 +0100

    add local functions for IEnumerable and Async

---
+++ CSharpGeneral/LocalFunctions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGeneral
+{
+    public class LocalFunctions
+    {
+        public static IEnumerable<int> OddSequence(int start, int end)
+        {
+            if (start < 0 || start > 99)
+                throw new ArgumentOutOfRangeException("start must be between 0 and 99.");
+            if (end > 100)
+                throw new ArgumentOutOfRangeException("end must be less than or equal to 100.");
+            if (start >= end)
+                throw new ArgumentException("start must be less than end.");
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 1)
+                    yield return i;
+            }
+        }
+
+        public static IEnumerable<int> OddSequenceWithLocalFunction(int start, int end)
+        {
+            if (start < 0 || start > 99)
+                throw new ArgumentOutOfRangeException("start must be between 0 and 99.");
+            if (end > 100)
+                throw new ArgumentOutOfRangeException("end must be less than or equal to 100.");
+            if (start >= end)
+                throw new ArgumentException("start must be less than end.");
+
+            return GetOddSequenceEnumerator();
+
+            IEnumerable<int> GetOddSequenceEnumerator()
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    if (i % 2 == 1)
+                        yield return i;
+                }
+            }
+        }
+
+        public static async Task<string> DownloadPage(string url)
+        {
+            if (url.Length < 5)
+                throw new ArgumentException("Invaid url");
+
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            return "html content";
+        }
+
+        /*
+         * Ordinarily, exceptions thrown in async method require that you examine the inner exceptions of an AggregateException. Local functions allow your code to fail fast and allow your exception to be both thrown and observed synchronously.
+         */
+        public static Task<string> DownloadPageWithLocalFunction(string url)
+        {
+            if (url.Length < 5)
+                throw new ArgumentException("Invaid url");
+
+            return GetPageAsync();
+
+            async Task<string> GetPageAsync()
+            {
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                return "html content";
+            }
+        }
+    }
+}
---
+++ CSharpTests/LocalFunctionsTests.cs
@@ -0,0 +1,59 @@
+﻿using CSharpGeneral;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSharpTests
+{
+    public class LocalFunctionsTests
+    {
+        [Fact]
+        public void WithoutLocalFunctionTest()
+        {
+            IEnumerable<int> oddNumbers = LocalFunctions.OddSequence(10, 109);
+
+            /*
+             * (w/o local functions) For method iterators, exceptions are surfaced only when the returned sequence is enumerated, and not when the iterator is retrieved.
+             */
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                foreach (var item in oddNumbers)
+                {
+                    Debug.Print(item.ToString());
+                }
+            });
+        }
+
+        [Fact]
+        public void WithLocalFunctionTest()
+        {
+            /*
+             * One of the useful features of local functions is that they can allow exceptions to surface immediately.
+             */
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                IEnumerable<int> oddNumbers = LocalFunctions.OddSequenceWithLocalFunction(10, 109);
+            });
+        }
+
+        [Fact]
+        public void WithOutLocalFunctionAsyncTest()
+        {
+            Assert.Throws<AggregateException>(() =>
+            {
+                var result = LocalFunctions.DownloadPage("x").Result;
+            });
+        }
+
+        [Fact]
+        public void WithLocalFunctionAsyncTest()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var result = LocalFunctions.DownloadPageWithLocalFunction("x").Result;
+            });
+        }
+    }
+}
