9a9844af51c305e644dd279a7d91f9c52c6c5191
Upendra Gusain <upendra.gusain@gmail.com>
2020-07-12 12:23:44 +0100

    add io abstraction package for testing filesystem logic

---
+++ CSharpGeneral/IOAbstractions.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.IO.Abstractions;
+
+namespace CSharpGeneral
+{
+    public class IOAbstractions
+    {
+        private readonly IFileSystem _fileSystem;
+        private readonly string _inputFilePath;
+        private readonly string _outputFilePath;
+
+        public IOAbstractions(IFileSystem fileSystem, string inputFilePath, string outputFilePath)
+        {
+            this._fileSystem = fileSystem;
+            this._inputFilePath = inputFilePath;
+            this._outputFilePath = outputFilePath;
+        }
+
+        public void ToUpperCaseFile()
+        {
+            using (StreamReader streamReader = _fileSystem.File.OpenText(_inputFilePath))
+            using (StreamWriter streamWriter = _fileSystem.File.CreateText(_outputFilePath))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    streamWriter.WriteLine(line.ToUpperInvariant());
+                }
+            }
+        }
+    }
+}
---
+++ CSharpTests/IOAbstracionsShould.cs
@@ -0,0 +1,33 @@
+﻿using CSharpGeneral;
+using System.IO.Abstractions.TestingHelpers;
+using Xunit;
+
+namespace CSharpTests
+{
+    public class IOAbstracionsShould
+    {
+        [Fact]
+        public void CreateNewFileWithUppercaseText()
+        {
+            var line1 = "This is the first line";
+            var mockInputFile = new MockFileData(line1+ "\nThis is the 2nd line\nLast line.");
+
+            var mockFileSystem = new MockFileSystem();
+            string inputFilePath = @"c:\in\file1.xt";
+            string outputFilePath = @"c:\out\file1.txt";
+            mockFileSystem.AddFile(inputFilePath, mockInputFile);
+            mockFileSystem.AddDirectory(@"c:\out");
+
+            var sut = new IOAbstractions(mockFileSystem, inputFilePath, outputFilePath);
+
+            sut.ToUpperCaseFile();
+
+            Assert.True(mockFileSystem.FileExists(outputFilePath));
+
+            MockFileData processedfile = mockFileSystem.GetFile(outputFilePath);
+            string[] lines = processedfile.TextContents.SplitLines();
+
+            Assert.Equal(line1.ToUpperInvariant(), lines[0]);
+        }
+    }
+}
