d798ea71ea403acf6e75d108711b1167e4bd5589
Upendra Gusain <upendra.gusain@gmail.com>
2020-07-13 12:30:34 +0100

    add fluent assertions tests

---
+++ CSharpGeneral/FluentAssertionsEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpGeneral
+{
+    public class FluentAssertionsEmployee
+    {
+        public string Name { get; }
+        public DateTime DateOfBirth { get; }
+
+        private readonly List<string> Achievements;
+
+
+        public FluentAssertionsEmployee(string name, DateTime dateOfBirth)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public int GetRemainingLeaves(int month)
+        {
+            return (12 - month) * 2;
+        }
+    }
+}
---
+++ CSharpTests/FluentAssertionsEmployeeShould.cs
@@ -0,0 +1,76 @@
+﻿using CSharpGeneral;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CSharpTests
+{
+    public class FluentAssertionsEmployeeShould: IClassFixture<FluentAssertionsEmployeeFixture>
+    {
+        private readonly FluentAssertionsEmployeeFixture _fixture;
+
+        public FluentAssertionsEmployeeShould(FluentAssertionsEmployeeFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void SetEmployeeName()
+        {
+            FluentAssertionsEmployee employee =
+                _fixture.Employee;
+
+            employee.Name.Should()
+                .Be("Upendra", because: "That's what my parents named me!");
+            employee.DateOfBirth.Should()
+                .Be(new DateTime(1913, 3, 23), because: "believe it or not, I am very very old!");
+        }
+
+        [Theory]
+        [MemberData(nameof(EmployeesData.Data), MemberType = typeof(EmployeesData))]
+        public void SetEmployeeNames(string name, DateTime dob)
+        {
+            FluentAssertionsEmployee employee =
+               new FluentAssertionsEmployee(name, dob);
+
+            employee.Name.Should()
+                .Be(name, because: "That's what my parents named me!");
+            employee.DateOfBirth.Should()
+                .Be(dob, because: "believe it or not, I am very very old!");
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(7)]
+        public void GetRemainingLeaves(int month)
+        {
+            FluentAssertionsEmployee employee =
+                new FluentAssertionsEmployee("", DateTime.Now);
+
+            employee.GetRemainingLeaves(month).Should()
+                .Be((12 - month) * 2);
+        }
+    }
+
+    public class FluentAssertionsEmployeeFixture
+    {
+        public readonly FluentAssertionsEmployee Employee;
+        public FluentAssertionsEmployeeFixture()
+        {
+            Employee = new FluentAssertionsEmployee("Upendra", new DateTime(1913, 3, 23));
+        }
+    }
+
+    public class EmployeesData
+    {
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                yield return new object[] {"Jon snow", new DateTime(1976, 2, 19) };
+                yield return new object[] { "Tyrion Lanister", new DateTime(1967, 12, 1) };
+            }
+        }
+    }
+}
