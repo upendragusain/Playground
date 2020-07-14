ea6c6fab2ee30b6706df4c66fffb7cad84730dd3
Upendra Gusain <upendra.gusain@gmail.com>
2020-07-14 16:14:09 +0100

    add delegate tests

---
+++ CSharpGeneral/Delegates/Play.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace CSharpGeneral.Delegates
+{
+    public class Play
+    {
+        public delegate string MyDelegate(string name);
+
+        public string UseGreetDelegate()
+        {
+            MyDelegate myDelegate = SayHi;
+            return myDelegate("Upendra");
+        }
+
+        public string SayHi(string name)
+        {
+            return $"Hi, {name}";
+        }
+
+        public string SayBye(string name)
+        {
+            return $"Bye, {name}";
+        }
+
+        public string UseDelegateAsParameter(MyDelegate del)
+        {
+            return del("I am a message from a delegate passed as a parameter!");
+        }
+
+        public void MulticastDelegate()
+        {
+            MyDelegate myMulticastDelegate = SayHi;
+            myMulticastDelegate += SayBye;
+
+            MyDelegate anonDelegate = (v) => { return v; };
+
+            MyDelegate del3 = myMulticastDelegate + anonDelegate;
+
+            var res = myMulticastDelegate("Jon Snow");
+
+            var res2 = anonDelegate("anonymous method delegate");
+
+            var res3 = del3("call all in added sequence, the result will be the result of last method invocation");
+
+            var length = del3.GetInvocationList().Length;
+        }
+    }
+}
---
+++ CSharpTests/DelegateTests.cs
@@ -0,0 +1,40 @@
+﻿using CSharpGeneral.Delegates;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpTests
+{
+    public class DelegateTests
+    {
+        private readonly Play play;
+        public DelegateTests()
+        {
+            play = new Play();
+        }
+
+        [Fact]
+        public void SimpleDelegateCall()
+        {
+            var result = play.UseGreetDelegate();
+            result.Should().Be("Hi, Upendra");
+        }
+
+        [Fact]
+        public void PassDelegateAsParameter()
+        {
+            var result = play.UseDelegateAsParameter(MyMethod);
+            result.Contains("Invoked").Should().Be(true);
+        }
+
+        private string MyMethod(string message)
+        {
+            return $"Invoked {nameof(MyMethod)}, received msg: {message}";
+        }
+
+        [Fact]
+        public void MulticastDelegate()
+        {
+            play.MulticastDelegate();
+        }
+    }
+}
