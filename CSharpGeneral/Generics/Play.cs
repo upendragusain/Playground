40a635ab16bd8404d27446c3aab68aef7933d2fb
Upendra Gusain <upendra.gusain@gmail.com>
2020-07-15 11:31:39 +0100

    generic with multiple constraints

---
+++ CSharpGeneral/Generics/Play.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace CSharpGeneral.Generics
+{
+    public class Play
+    {
+        public static void MultipleConstraintsOnAGenericType()
+        {
+            EmployeeList<PartTimer> partTiers = new EmployeeList<PartTimer>();
+        }
+
+    }
+
+    public class EmployeeList<T> where T : Employee, IEmployee, IComparable<T>, new()
+    {
+    }
+
+    public interface IEmployee
+    {
+        string Do();
+    }
+
+    public class Employee
+    {
+        public virtual string DoWork()
+        {
+            return "Done";
+        }
+    }
+
+
+    public class PartTimer : Employee, IEmployee, IComparable<PartTimer>
+    {
+        public int Experience { get; set; }
+
+        //uncommenting the below will not compile as then no more public parameterless ctor fpr the constraint
+        //public PartTimer(int id)
+        //{
+
+        //}
+
+        public override string DoWork()
+        {
+            return $"{nameof(PartTimer)}.{nameof(DoWork)}-Done";
+        }
+
+        public int CompareTo([AllowNull] PartTimer other)
+        {
+            return other.Experience > this.Experience ? 1 : 0;
+        }
+
+        public string Do()
+        {
+            return "Did";
+        }
+    }
+}
---
+++ CSharpTests/GenericsTests.cs
@@ -0,0 +1,17 @@
+﻿using CSharpGeneral.Generics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CSharpTests
+{
+    public class GenericsTests
+    {
+        [Fact]
+        public void MultipleConstraintsOnAGenericType_Test()
+        {
+            Play.MultipleConstraintsOnAGenericType();
+        }
+    }
+}
